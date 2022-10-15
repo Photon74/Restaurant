6e8a5f9866197a4515ffb746ad349c93ca1f7271
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-10-15 20:03:13 +0300

    Booking methods

--- Restaurant.Booking/Restaurant.cs
+++ Restaurant.Booking/Restaurant.cs
@@ -9,6 +9,7 @@ namespace Restaurant.Booking
     internal class Restaurant
     {
         private readonly List<Table> _tables = new();
+        private readonly int delay = 5000;
 
         public Restaurant()
         {
@@ -16,6 +17,39 @@ namespace Restaurant.Booking
             {
                 _tables.Add(new Table(i));
             }
+        }
+
+        public void BookFreeTable(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду, я подберу столик и подтвержу вашу бронь, оставайтесь на линии.");
+
+            var table = _tables.FirstOrDefault(t => 
+                                                    t.SeatsCount >= countOfPersons
+                                                    && t.State == State.Free);
+            Thread.Sleep(delay);
+            table?.SetState(State.Booked);
+
+            Console.WriteLine(table is null
+                ? "К сожалению все столики заняты."
+                : $"Готово! Ваш столик номер {table.Id}");
+        }
+
+        public void BookFreeTableAsync(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду, я подберу столик и подтвержу вашу бронь, Вам придет уведомление.");
+
+            Task.Run(async () =>
+            {
+                var table = _tables.FirstOrDefault(t =>
+                                                    t.SeatsCount >= countOfPersons
+                                                    && t.State == State.Free);
+                await Task.Delay(delay);
+                table?.SetState(State.Booked);
+
+                Console.WriteLine(table is null
+                ? "УВЕДОМЛЕНИЕ: К сожалению все столики заняты."
+                : $"УВЕДОМЛЕНИЕ: Готово! Ваш столик номер {table.Id}");
+            });
         }
     }
 }
