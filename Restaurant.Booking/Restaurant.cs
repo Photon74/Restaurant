8fdb5e2b1c511da17409002fb5267fac56e68be3
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-10-15 18:28:29 +0300

    Restaurant

---
+++ Restaurant.Booking/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking
+{
+    internal class Restaurant
+    {
+        private readonly List<Table> _tables = new();
+
+        public Restaurant()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _tables.Add(new Table(i));
+            }
+        }
+    }
+}
